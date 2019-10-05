e08440d79609d72e3f19e1e9c4f38f1271012b0a
koblevmurat <koblevmurat@gmail.com>
2019-10-05 23:15:56 +0300

    Update Main.java

--- src/Lessons/Geekbrains/Algorithms/Main.java
+++ src/Lessons/Geekbrains/Algorithms/Main.java
@@ -3,6 +3,9 @@ package Lessons.Geekbrains.Algorithms;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Hello world!");
+    // write your code here
+
     }
 }
