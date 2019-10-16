004b76f785d0e508c44a268484f363f2deef0e8d
Moor <KoblevMurat@gmail.com>
2019-10-16 05:20:23 +0300

    связанные списки

--- src/Lessons/Geekbrains/Algorithms/Lesson4/LLElement.java
+++ src/Lessons/Geekbrains/Algorithms/Lesson4/LLElement.java
@@ -1,4 +1,28 @@
 package Lessons.Geekbrains.Algorithms.Lesson4;
 
 public class LLElement {
+
+
+    private int Value;
+    private LLElement next;
+
+    public LLElement(int _val){
+        this.Value = _val;
+    }
+
+    public int getValue() {
+        return Value;
+    }
+
+    public void setValue(int value) {
+        Value = value;
+    }
+
+    public LLElement getNext() {
+        return next;
+    }
+
+    public void setNext(LLElement next) {
+        this.next = next;
+    }
 }
