74bb9b05194ee63f42c68356916aba22fddbc330
koblevmurat <koblevmurat@gmail.com>
2019-10-17 10:53:28 +0300

    дз

--- src/Lessons/Geekbrains/Algorithms/Lesson5/RecursivePwr.java
+++ src/Lessons/Geekbrains/Algorithms/Lesson5/RecursivePwr.java
@@ -1,4 +1,25 @@
 package Lessons.Geekbrains.Algorithms.Lesson5;
 
 public class RecursivePwr {
+
+    public static double Power(int _val, int _pwr) {
+        if (_pwr == 0){
+            return 1;
+        }
+        else
+        {
+            if (_pwr>0)
+                return _val * Power(_val, --_pwr);
+            else {
+                _pwr = -1 * _pwr;
+                double res = Power(_val, _pwr);
+                return 1 / res;
+            }
+        }
+    }
+
+    public static void Test(){
+        System.out.println(Power(2, 2));
+        System.out.println(Power(2, -2));
+    }
 }
