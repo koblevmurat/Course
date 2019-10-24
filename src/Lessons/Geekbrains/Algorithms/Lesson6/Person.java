a5191342a151a58a0eb07664007262cf32461c08
Moor <KoblevMurat@gmail.com>
2019-10-24 13:02:49 +0300

    дз на проверку, урок 6

--- src/Lessons/Geekbrains/Algorithms/Lesson6/Person.java
+++ src/Lessons/Geekbrains/Algorithms/Lesson6/Person.java
@@ -1,4 +1,16 @@
 package Lessons.Geekbrains.Algorithms.Lesson6;
 
+import javax.naming.Name;
+
 public class Person {
+
+    String name;
+    int id;
+    int age;
+
+    public Person(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+    }
 }
