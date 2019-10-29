cf8776cdf55131a17d6dfc79d351ef3f593bf34c
Moor <KoblevMurat@gmail.com>
2019-10-29 23:22:44 +0300

    тесты

--- src/Lessons/Geekbrains/Algorithms/Lesson8/MyHashMap.java
+++ src/Lessons/Geekbrains/Algorithms/Lesson8/MyHashMap.java
@@ -1,4 +1,23 @@
 package Lessons.Geekbrains.Algorithms.Lesson8;
 
 public class MyHashMap {
+
+    char [] Map;
+    int Size;
+
+    public MyHashMap(int _Size){
+        this.Size = _Size;
+        this.Map = new char[Size];
+    }
+
+    public void AddValue(int _key, char _val){
+        int ix = _key%Size;
+        Map[ix] = _val;
+    }
+
+    public char GetVal(int _key){
+        int ix = _key%Size;
+        return Map[ix];
+    }
+
 }
